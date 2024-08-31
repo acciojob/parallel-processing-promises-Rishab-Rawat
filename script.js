a5b225c57420aa53d423c91f19d6ebc9a3c23a48
Rishab-Rawat <rawatrishab66@gmail.com>
2024-08-31 17:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  const downloadImage = (image) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.onload = () => resolve(img); 
+      img.onerror = () => reject(new Error(Failed to load image's URL: ${image.url}));
+    });
+  };
+
+  Promise.all(images.map(downloadImage))
+    .then((loadedImages) => {
+      output.innerHTML = "";
+      loadedImages.forEach((img) => output.appendChild(img));
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+});
